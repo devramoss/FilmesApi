c8a4324747c6dadced6e72dcf0110409cb24e762
devramoss <devramoss2003@gmail.com>
2025-01-15 07:40:09 -0300

    Atualizando filme

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,7 @@
-﻿using FilmesApi.Models;
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -7,30 +10,57 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static int Id = 0;
-    private static List<Filme> filmes = new List<Filme>();
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public FilmeController(FilmeContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
     {
-        filme.Id = Id++;
-        filmes.Add(filme);
+        Filme filme = _mapper.Map<Filme>(filmeDto);
 
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id}, filme);
     }
 
     [HttpGet]
 
-    public IEnumerable<Filme> RecuperaFilmes()
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return  filmes;
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
-    public Filme ? RecuperaFilmePorId(int id)
+    public IActionResult RecuperaFilmePorId(int id)
     {
-       return filmes.FirstOrDefault(filme => filme.Id == id);
+       var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+       if(filme == null)
+            return NotFound();
+
+       return Ok(filme);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null)
+            return NotFound();
+
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+
     }
 
 }
---
+++ FilmesApi/Models/Profiles/FilmeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+
+namespace FilmesApi.Models.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile() {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<UpdateFilmeDto, Filme>();
+        }
+    }
+}
